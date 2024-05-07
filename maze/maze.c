3aeb6cdedc707dcc4d36405f1a18777d70ee1b78
mengxin114 <1287961101@qq.com>
2024-05-07 17:21:45 +0800

    load map and check data

--- maze/maze.c
+++ maze/maze.c
@@ -1,8 +1,136 @@
-#ifndef __TASKOP_H__
-#define __TASKOP_H__
-#include <stdlib.h>
-#include <stdio.h>
-#define LINE_MAX 1024
+#include "maze.h"
+
+int main(int argc, char* argv[])
+{
+	load(argv[1]);
+	while (!win)
+	{
+		in = getInput();
+		switch (in)
+		{
+		case '\0':
+			printf("wrong input");
+			break;
+		case 'M':
+			break;
+		case 'Q':
+			break;
+		default:
+			break;
+		}
+	}
+}
 
 
-#endif
+
+
+
+void load(char fileName[])
+{
+	char buf[LINE_MAX] = { 0 };
+	FILE* f;
+	f = fopen(fileName, "r");
+	if (f == NULL)
+	{
+		printf("Error: cannot find file");
+		exit(-1);
+	}
+	//read height and weight
+	fgets(buf, LINE_MAX, f);
+	width = strlen(buf);
+	height += 1;
+	while (fgets(buf, LINE_MAX, f) != NULL)
+	{
+		height += 1;
+	}
+	width -= 1;
+	rewind(f);
+	//create maze
+	creatMaze();
+	//load map
+	
+	for (int j = 0;j < height; j++)
+	{
+		fgets(buf, LINE_MAX, f);
+		checkData(buf);
+		for (int i = 0; i < width; i++) {
+			map[j][i] = buf[i];
+		}
+	}
+	printMap();
+	printf("%c", map[height - 1][width - 1]);
+	printf("%c", map[height - 2][width - 1]);
+}
+
+void printMap()
+{
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			printf("%c", map[j][i]);
+		}
+		printf("\n");
+	}
+}
+
+void creatMaze()
+{
+	printf("%d,%d\n", height, width);
+	map = (char**)malloc(sizeof(char*) * height);
+	for (int j = 0; j < height; j++) {
+		map[j] = (char*)malloc(sizeof(char) * width);
+	}
+
+
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			map[j][i] = 'a';
+		}
+	}
+}
+
+void checkData(char buf[])
+{
+	if (strlen(buf) - 1 != width)
+	{
+		printf("buf长度不对");
+		exit(-3);
+	}
+	for (int i = 0; i < width; i++)
+	{
+		if (buf[i] == 'S' || buf[i] == 'E')
+		{
+			count += 1;
+		}
+		else if (buf[i] == ' ' || buf[i] == '#')
+		{
+
+		}
+		else if (count > 2)
+		{
+			printf("invalid char");
+			exit(-3);
+		}
+		else
+		{
+			printf("invalid char");
+			exit(-3);
+		}
+	}
+}
+
+
+char getInput()
+{
+	char input;
+	input = scanf("%c", &input);
+	input = toupper(input);
+	char a[6] = { 'W', 'A', 'S', 'D', 'Q','M' };
+	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+	{
+		if (input == a[i])
+		{
+			return input;
+		}
+	}
+	return '\0';
+}
--- maze/maze.h
+++ maze/maze.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef __TASKOP_H__
+#define __TASKOP_H__
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#define LINE_MAX 1024
+int width;
+int height;
+char** map;
+int count = 0;
+int win = 0;
+char in = '\0';
+void load(char fileName[]);
+
+void creatMaze();
+char getInput();
+void printMap();
+void checkData(char buf[]);
+
+
+
+
+
+
+
+#endif
